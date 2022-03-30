6959307fca7b398d880393f67848921c43ca3d0a
georgek2 <george22k44@gmail.com>
2022-03-30 23:15:35 +0300

    Gene

---
+++ Files/Projects/gene_data/trial.py
@@ -0,0 +1,57 @@
+import argparse
+from assign import my_io
+
+def create_dic(fh_in):
+    """
+    Create a dictionary to store gene categories their and descriptions
+    """
+    desc = {}
+    with open(fh_in, 'r') as file:
+        lines = file.readlines()[1:]
+    for line in lines:
+        gene = line.strip().split("\t")[0].lower()
+        description = line.strip().split("\t")[1].lower()
+        desc[gene] = description
+    return desc
+
+create_dic('chr21_genes.txt')
+
+
+def get_cli_args():
+    """
+    Just get the command line options using argparse
+    @return: Instance of argparse arguments
+    """
+    parser = argparse.ArgumentParser(description='Open chr21_genes.txt, \
+    and ask user for a gene name')
+
+    parser.add_argument('-i', '--infile', dest='infile',
+        type=str, help='Path to the file to open',
+        required=False, default='./chr21_genes.txt')
+
+    return parser.parse_args()
+
+
+def main():
+    args = get_cli_args()
+    fh_in = args.infile
+    desc = create_dic(fh_in)
+
+    while True:
+        gene = input('Enter the gene symbol or quit to exit: ')
+
+        if gene in desc:
+            print('Gene found...')
+            print('Desc: ', desc[gene])
+            break
+        elif gene == 'quit':
+            print('Exited...')
+            break
+        else:
+            print('Not found...')
+
+
+main()
+
+
+
